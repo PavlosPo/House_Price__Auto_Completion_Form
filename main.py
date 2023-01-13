478026296f0356e7f0087c68b543c547c7b33fe4
PavlosPo <paulospoulos7@gmail.com>
2023-01-13 19:36:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
 from bs4 import BeautifulSoup
 import requests
+from pprint import pprint
 
 form_link = 'https://forms.gle/5HMoiNMvmdMrmGCK6'
 house_link = 'https://www.zillow.com/homes/for_rent/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-122.58610762548828%2C%22east%22%3A-122.28055037451172%2C%22south%22%3A37.66503360680628%2C%22north%22%3A37.88538511109395%7D%2C%22mapZoom%22%3A12%2C%22isMapVisible%22%3Afalse%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D'
@@ -18,10 +19,11 @@ def find_houses():
     """ It finds the houses as a list. Returns a list of html code"""
     response = requests.get(url=house_link, headers=headers).text
     soup = BeautifulSoup(response, "lxml")
-    houses_list = soup.find(name="ul",
-                            class_="List-c11n-8-81-1__sc-1smrmqp-0 srp__sc-1psn8tk-0 dXyjtp photo-cards with_constellation")
-    houses = houses_list.find_all(name="div",
-                                  class_="StyledPropertyCardDataWrapper-c11n-8-81-1__sc-1omp4c3-0 fEStTH property-card-data")
-    return houses
-
+    all_houses_list = soup.find(name="ul", class_="List-c11n-8-81-1__sc-1smrmqp-0 srp__sc-1psn8tk-0 dXyjtp photo-cards with_constellation")
+    section_of_houses = all_houses_list.find_all(name="li",
+                                  class_="ListItem-c11n-8-81-1__sc-10e22w8-0 srp__hpnp3q-0 enEXBq with_constellation")
+    # Itterate per house
+    for house in section_of_houses:
+        address = house.find(name="a", class_="StyledPropertyCardDataArea-c11n-8-81-1__sc-yipmu-0 lpqUkW property-card-link").getText()
+        pprint(address)
 find_houses()
